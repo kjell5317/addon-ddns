7674c676c5d74dca0577dc4eae70099d8c5f748b
kjell <kjell5317@gmail.com>
2021-08-05 12:56:01 +0200

    update when ip changes

--- run.sh
+++ run.sh
@@ -7,27 +7,31 @@ zone=$(bashio::config 'zone')
 api_key=$(bashio::config 'api_key')
 time=$(bashio::config 'update')
 
+ip1=$(dig +short myip.opendns.com @resolver1.opendns.com)
+
 while true; do
 
-    ip=$(dig +short myip.opendns.com @resolver1.opendns.com)
-    bashio::log.info "$ip"
+    if ip2=$(dig +short myip.opendns.com @resolver1.opendns.com) \
+    && [ $ip1 != $ip2 ]; then
 
-    if answer=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone/dns_records?type=A&match=all" \
-        -H "Authorization: Bearer $api_key" \
-        -H "Content-Type: application/json") \
-        && [ $(echo $answer | jq -r '.success') == 'true' ]; then
-            domains=$(echo $answer | jq -r '.result')
-    else bashio::log.error "Failed getting records $(echo $answer | jq -r 'errors')"; fi
-
-    for i in $(echo $domains | jq -r '.[] | @base64'); do
-        if answer=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone/dns_records/$(echo $i | base64 -d | jq -r '.id')" \
+        if answer=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/$zone/dns_records?type=A&match=all" \
             -H "Authorization: Bearer $api_key" \
-            -H "Content-Type: application/json" \
-            -d '{"content": "'$ip'", "name": "'$(echo $i | base64 -d | jq -r '.name')'", "type": "A", "ttl": 1, "proxied": true}') \
+            -H "Content-Type: application/json") \
             && [ $(echo $answer | jq -r '.success') == 'true' ]; then
-            bashio::log.info "Updated $(echo $i | base64 -d | jq -r '.name')"
-        else bashio::log.error "Failed updating $(echo $i | base64 -d | jq -r '.name') $(echo $answer | jq -r '.errors | .[0]')"; fi
-    done
+                domains=$(echo $answer | jq -r '.result')
+        else bashio::log.error "Failed getting records $(echo $answer | jq -r 'errors')"; fi
 
+        for i in $(echo $domains | jq -r '.[] | @base64'); do
+            if answer=$(curl -s -X PUT "https://api.cloudflare.com/client/v4/zones/$zone/dns_records/$(echo $i | base64 -d | jq -r '.id')" \
+                -H "Authorization: Bearer $api_key" \
+                -H "Content-Type: application/json" \
+                -d '{"content": "'$ip'", "name": "'$(echo $i | base64 -d | jq -r '.name')'", "type": "A", "ttl": 1, "proxied": true}') \
+                && [ $(echo $answer | jq -r '.success') == 'true' ]; then
+                bashio::log.info "Updated $(echo $i | base64 -d | jq -r '.name')"
+            else bashio::log.error "Failed updating $(echo $i | base64 -d | jq -r '.name') $(echo $answer | jq -r '.errors | .[0]')"; fi
+        done
+        ip1="$ip2"
+    fi
+    bashio::log.info "$ip1 $ip2"
     sleep "$time"
 done
